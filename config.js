7373cd8d080f432502aa4c1c25be1e592519b7f0
NihalNair <86263935+NihalNair@users.noreply.github.com>
2022-06-15 21:55:49 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+ import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQvOtKXWwEVvCl_VuhENay8utKz1GGBbA",
+  authDomain: "project-60-f60de.firebaseapp.com",
+  projectId: "project-60-f60de",
+  storageBucket: "project-60-f60de.appspot.com",
+  messagingSenderId: "203062345513",
+  appId: "1:203062345513:web:401bfed522d81d9bb78138"
+};
+firebase.initializeApp(firebaseConfig)
+  export default firebase.database()
+ 
+
+  
